f0f4d1f033d755c39c992af676d7bb42be4ddb6a
blogdopata <vcaballero_c@homail.com>
2020-01-31 17:56:29 -0500

    fix widget

--- wp-content/plugins/gymfitness_widgets/gymfitness_widgets.php
+++ wp-content/plugins/gymfitness_widgets/gymfitness_widgets.php
@@ -12,110 +12,83 @@
 */
 if(!defined('ABSPATH')) die(); // Para q nadie pueda acccedeer  a este  codigo
  
-
 /**
- * GymFitness_widget Class
+ * Adds GymFitness_Class_widget.
  */
-class GymFitness_widget extends WP_Widget {
-    /** constructor */
-    function GymFitness_widget() {
-        parent::WP_Widget(false, $name = 'GymFitness_widget');	
-    }
+class GymFitness_Class_widget extends WP_Widget {
 
-    /** @see WP_Widget::widget */
-    function widget($args, $instance) {		
-        extract( $args );
-        $title = apply_filters('GymFitness Clases', $instance['title']);
-        ?>
-        <?php echo $before_widget; ?>
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'foo_widget', // Base ID
+			esc_html__( 'GymFitness Clases', 'text_domain' ), // Name
+			array( 'description' => esc_html__( 'agrega clases en el widget', 'text_domain' ), ) // Args
+		);
+	}
 
-        <?php if ( $title )
-                                echo $before_title . $title . $after_title;
-                                
-                                
-                                
-                                ?>
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
+		}
+		echo esc_html__( 'Hello, World!', 'text_domain' );
+		echo $args['after_widget'];
+	}
 
-        <ul>
-            <?php  $args= array(
-                                'post_type' => 'gymfitness_clases',
-                                'posts_per_page' => 3,
-                               // 'orderby' => 'rand'
-                                );
-                                $clases= new WP_Query($args);
-                                while($clases->have_posts()): $clases -> the_post();
-                                ?>
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'text_domain' );
+		?>
+		<p>
+		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'text_domain' ); ?></label> 
+		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
 
-            <li class="class-sidebar">
-                <div class="imagen">
-                    <?php the_post_thumbnail('thumbnail') ;?>
-                </div>
-                <div class="contenido-clase">
-                    <a href="<?php the_permalink();  ?>">
-                        <h3><?php the_title(); ;?></h3>
-                    </a>
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
 
-                    <?php 
+		return $instance;
+	}
 
-                                        $hora_inicio =  get_field('hora_inicio');
-                                        $hora_fin=  get_field('hora_fin');
-                                    ?>
-
-
-                    <p><?php the_field('dias_clase') ;?> - <?php echo $hora_inicio . " a " . $hora_fin ;?> </p>
-                </div>
-            </li>
-
-            <?php endwhile; wp_reset_postdata();?>
-
-        </ul>
-
-
-
-<?php echo $after_widget; ?>
-<?php
-    }
-
-
-    /** @see WP_Widget::update */ /* Contenido que se va a mmostrar en el panel de control */
-
-
-    function update($new_instance, $old_instance) {				
-	$instance = $old_instance;
-	$instance['cantidad'] = strip_tags($new_instance['cantidad']);
-        return $instance;
-    }
-
-
-
-
-    /** @see WP_Widget::form */
-    public function form($instance) {	
-        $cantidad = !empty($instance['cantidad']) ? $instance['cantidad'] : esc_html__('¿Cuantas clases debeas mostrar?' ,'gymfitness');  ?>
-        <p> 
-            <label for="<?php echo esc_attr( $this->get_field_id('cantidad') ) ?>">
-                <?php esc_attr_e('¿Cuántas Clases deseas mostrar?', 'gymfitness') ;?>
-            
-            </label>
-            <input type="number" 
-            
-                class="widefat"
-                id="<?php echo esc_attr( $this->get_field_id('cantidad') ) ?>"
-                name="<?php echo esc_attr( $this->get_field_id('cantidad') ) ?>"
-                value="<?php echo esc_attr( $cantidad) ?>"
-            >
-
-        </p>
-
-<?php 
-    }
-
-} // clase FooWidget
+} // class Foo_Widget
 
 
 
 // registrar el widget FooWidget
-add_action('widgets_init', create_function('', 'return register_widget("GymFitness_widget");'));
+
+function registrar_Gyfitenesss_widget() {
+    register_widget( 'GymFitness_Class_widget' );
+}
+add_action( 'widgets_init', 'registrar_Gyfitenesss_widget' );
 
 
 
